f407065a23270ba8d8794eee6f7e7011f95e8e0e
italoEng <franca.italo05@gmail.com>
2022-02-13 17:25:03 -0300

    commit

--- calculos.py
+++ calculos.py
@@ -1,1 +1,11 @@
-print("hello world")
+
+
+def paralelo(*args):
+    """
+    função para calcular resistores em paralelo
+    """
+    n = len(args) # numero de resistores
+    den = 0
+    for i in range(n):
+        den = 1/args[i-1] + den # denominador
+    return 1/den
