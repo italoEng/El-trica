1feb661cb6d9b4ad2cba4390f44bafe0be9c7d34
italoEng <franca.italo05@gmail.com>
2022-03-02 21:54:03 -0300

    commit

--- calculos.py
+++ calculos.py
@@ -9,3 +9,19 @@ def paralelo(*args):
     for i in range(n):
         den = 1/args[i-1] + den # denominador
     return 1/den
+
+def lei1(v="v",i="i",r="r"):
+    """
+    função que cálcula a primeira lei de ohm.\n
+    v = tensão elétrica em (V) \n
+    i = corrente elétrica em (A) \n
+    r = resistencia elétrica em (Ω) \n
+    """
+    if v == "v":
+        resposta = r*i
+    elif i == "i":
+        resposta = v/r
+    else:
+        resposta = v/i
+
+    return resposta
